b9f24c33fed68081d7960ce530bbef28a50b8cd7
Wcandyme <llbarkat@yahoo.com>
2023-02-05 00:35:16 -0500

    Update onionring-variables.js

--- onionring-variables.js
+++ onionring-variables.js
@@ -19,7 +19,7 @@ var ringName = 'Sadbook Ring';
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
  2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
-var ringID = 'my-ring';
+var ringID = 'sadbook-ring';
 
 //should the widget include a link to an index page?
 var useIndex = false;
